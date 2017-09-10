b052a68117d00d038a3b9ae449f5b1a21e34f80f
vbugaevsky <bugaevsky@mail.ru>
2017-09-10 22:40:20 +0300

    add main for mp3_to_wav

--- utils/mp3_to_wav.py
+++ utils/mp3_to_wav.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from pydub import AudioSegment
 
 
@@ -8,3 +9,8 @@ def mp3_to_wav(path_music):
     sound = AudioSegment.from_mp3(path_music)
     sound.export(path_music[:-4] + ".wav", format="wav")
     return path_music + ".wav"
+
+
+if __name__ == "__main__":
+    for music in sys.argv[1:]:
+        mp3_to_wav(music)
