51b0f422a4ff3a40aad86e7ec04e0fb1e14876ce
vbugaevsky <bugaevsky@mail.ru>
2017-09-10 21:40:04 +0300

    added convert seconds to ticks

--- utils/wav_file_wrapper.py
+++ utils/wav_file_wrapper.py
@@ -43,6 +43,9 @@ class WavFile:
         """
         return self.samples.shape[1]
 
+    def convert_seconds_to_ticks(self, secs):
+        return secs * self.rate
+
     def get_sub_track(self, start_sec=None, end_sec=None, channel=0):
         r"""
         :param start_sec: start position in seconds. if None, treat as start_sec = 0 seconds
@@ -56,8 +59,8 @@ class WavFile:
         if start_sec is not None and end_sec is not None and start_sec >= end_sec:
             raise Exception("Start should be less then end")
 
-        l_border = 0 if start_sec is None else start_sec * self.rate
-        r_border = len(self) if end_sec is None else end_sec * self.rate
+        l_border = 0 if start_sec is None else self.convert_seconds_to_ticks(start_sec)
+        r_border = len(self) if end_sec is None else self.convert_seconds_to_ticks(end_sec)
         return self.samples[l_border:r_border, channel]
 
     def split(self, part_time):
@@ -65,19 +68,19 @@ class WavFile:
         :param part_time: count of second in each part
         """
         l_border = 0
-        r_border = len(self) * self.rate
-        print r_border, part_time * self.rate
+        r_border = self.samples.shape[0]
+        print r_border, self.convert_seconds_to_ticks(part_time)
         print self.samples.shape
         part_begin = 0
         idx = 0
         # print self.get_channels()
-        while part_begin + part_time * self.rate <= r_border:
+        while part_begin + self.convert_seconds_to_ticks(part_time) <= r_border:
             wav.write(self.file_name[:-4] + "_" + str(idx) + self.file_name[-4:], self.rate, \
-                      self.samples[part_begin:part_begin + part_time * self.rate])
+                      self.samples[part_begin:part_begin + self.convert_seconds_to_ticks(part_time)])
             print idx
-            print self.samples[part_begin:part_begin + part_time * self.rate].shape
+            print self.samples[part_begin:part_begin + self.convert_seconds_to_ticks(part_time)].shape
             idx += 1
-            part_begin += part_time * self.rate
+            part_begin += self.convert_seconds_to_ticks(part_time)
 
 
 if __name__ == "__main__":
