c0707c3ca129d044e5e7b4b91a3287865f36c7cb
vbugaevsky <bugaevsky@mail.ru>
2017-09-10 23:05:24 +0300

    removed redundant 'r' in doc-strings

--- utils/wav_file_wrapper.py
+++ utils/wav_file_wrapper.py
@@ -9,9 +9,8 @@ import scipy.io.wavfile as wav
 
 class WavFile:
     def __init__(self, file_name, scale=False):
-        r"""
+        """
         :param file_name: Input file name for "*.wav"
-
         :param scale: Scale track values in [-1, 1]
         """
         self.file_name = file_name
@@ -31,15 +30,14 @@ class WavFile:
                 raise Exception("Wrong file format!")
 
     def __len__(self):
-        r"""
+        """
         :return: track's length in seconds
         """
         return self.samples.shape[0] / self.rate
 
     def get_channels(self):
-        r"""
+        """
         Number of channels in track. Usually equals 2 (left and right ears).
-
         :return: nb_channels
         """
         return self.samples.shape[1]
@@ -48,13 +46,10 @@ class WavFile:
         return secs * self.rate
 
     def get_sub_track(self, start_sec=None, end_sec=None, channel=0):
-        r"""
+        """
         :param start_sec: start position in seconds. if None, treat as 0 seconds
-
         :param end_sec: finish position in seconds. if None, treat as end_of_track in seconds
-
         :param channel: channel (usually 0 or 1)
-
         :return: part of track's channel, starting form start_sec and finishing end_sec
         """
         if start_sec is not None and end_sec is not None and start_sec >= end_sec:
