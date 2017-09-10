b37d778082a9e324f800f7bbd27eb0733f3a68c2
DanaZL <dana_zl@mail.ru>
2017-09-10 22:28:03 +0300

    -

--- utils/preprocess_dataset.py
+++ utils/preprocess_dataset.py
@@ -14,6 +14,19 @@ def add_postfix(dir_name, postfix):
         # print dir_name + f
         os.rename(dir_name + f, dir_name + f[:-4] + postfix + ".mp3")
 
+def split_audio(dir_name,  part_time):
+    """
+    Split each audio in directory to parts with part_time long
+    :param dir_name: directory name
+    :param part_time: count second in each part
+    :return: splitted files in dir_name
+    """
+    files = os.listdir(dir_name)
+    for f in tqdm(files):
+        mp3_to_wav(dir_name + f)./
+        x = WavFile(dir_name + f)
+        x.split(part_time)
+
 
 # add_postfix("../data/Deam/audio/", "D")
 # add_postfix("../data/1000S/clips_45seconds/", "S")
