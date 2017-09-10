6b07f1a9dd40105c41b8a028713c739931d5486c
vbugaevsky <bugaevsky@mail.ru>
2017-09-10 17:48:36 +0300

    Added wrapper for *.wav files

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__ = ["wav_file_wrapper" "preprocess_dir"]
--- utils/wav_file_wrapper.py
+++ utils/wav_file_wrapper.py
@@ -4,11 +4,15 @@
 import re
 import numpy as np
 import scipy.io.wavfile as wav
-import matplotlib.pyplot as plt
 
 
 class WavFile:
     def __init__(self, file_name, scale=False):
+        r"""
+        :param file_name: Input file name for "*.wav"
+
+        :param scale: Scale track values in [-1, 1]
+        """
         self.file_name = file_name
         self.rate, self.samples = wav.read(file_name)
 
@@ -21,36 +25,35 @@ class WavFile:
                 raise Exception("Wrong input format")
 
     def __len__(self):
+        r"""
+        :return: track's length in seconds
+        """
         return self.samples.shape[0] / self.rate
 
     def get_channels(self):
+        r"""
+        Number of channels in track. Usually equals 2 (left and right ears).
+
+        :return: nb_channels
+        """
         return self.samples.shape[1]
 
     def get_sub_track(self, start_sec=None, end_sec=None, channel=0):
+        r"""
+        :param start_sec: start position in seconds. if None, treat as start_sec = 0 seconds
+
+        :param end_sec: finish position in seconds. if None, treat as end_sec = end_of_track in seconds
+
+        :param channel: channel (usually 0 or 1)
+
+        :return: part of track's channel, starting form start_sec and finishing end_sec
+        """
         if start_sec is not None and end_sec is not None and start_sec >= end_sec:
             raise Exception("Start should be less then end")
+
         l_border = 0 if start_sec is None else start_sec * self.rate
         r_border = len(self) if end_sec is None else end_sec * self.rate
         return self.samples[l_border:r_border, channel]
 
-
-def create_spectrogram(sample, sample_rate):
-    figure = plt.figure(frameon=False)
-    spectrum, freqs, time, image = plt.specgram(
-        x=sample, NFFT=1024, noverlap=512, Fs=sample_rate, sides='onesided', cmap='jet'
-    )
-    print time.shape, freqs.shape
-    return figure
-
-
-wav_file = WavFile("Linkin Park_-_Numb.mp3.wav", scale=True)
-print len(wav_file)
-
-figure = create_spectrogram(wav_file.get_sub_track(start_sec=15, end_sec=25, channel=0), wav_file.rate)
-figure.savefig("ocean_0.png", bbox_inches="tight")
-
-wav_file = WavFile("ocean.wav", scale=True)
-print len(wav_file)
-
-figure = create_spectrogram(wav_file.get_sub_track(start_sec=15, end_sec=200, channel=0), wav_file.rate)
-figure.savefig("ocean_1.png", bbox_inches="tight")
+if __name__ == "__main__":
+    pass
