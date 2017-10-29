b72faa49458e9fdb78c076300bc95103c4eab716
Vladimir Bugaevskii <bugaevsky@mail.ru>
2017-10-29 13:49:33 +0300

    Sorted predictions

--- experiments/one_second_shift/test/predict.py
+++ experiments/one_second_shift/test/predict.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import shutil
+import operator
 
 sys.path.insert(0, os.getenv("HOME") + "/workdir/MusicMap/")
 
@@ -27,6 +28,8 @@ for root, dirs_list, files_list in os.walk(SPECTRO):
         print dir_spectros
         
         preds, names = net.predict(dir_spectros, os.path.join(RESULTS, "{}.csv".format(track_name)))
+        preds = np.asarray([x for x, _ in sorted(zip(preds, names), key=operator.itemgetter(1))])
+        names = sorted(names)
 
         show_on_map(preds[:, 0], preds[:, 1], names, os.path.join(RESULTS, "{}.png".format(track_name)))
 
--- utils/visualization.py
+++ utils/visualization.py
@@ -13,7 +13,7 @@ def show_on_map(valence_list, arousal_list, annotations, image_name, figsize=(15
     image = plt.imread(project_dir + 'utils/va_scale.jpg')
     plt.imshow(image, extent=[1, 9, 1, 9])
 
-    plt.plot(valence_list, arousal_list, 'ro', markersize='8')
+    plt.plot(valence_list, arousal_list, '-ro', markersize='8')
     for i, xy in enumerate(zip(valence_list, arousal_list)):
         ax.annotate(annotations[i], xy, textcoords='data')
     
